5e1fe8eca0e455c43b175351919f00d0043eb1b7
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-03-26 21:11:29 +0530

    Update sensor-validate.cpp

--- sensor-validate.cpp
+++ sensor-validate.cpp
@@ -9,11 +9,12 @@ bool isDifferenceOkay(double value, double nextValue, double maxDelta) {
 bool generalValidation(double* values, int numOfValues, typeOfReading type) {
   int lastButOneIndex = numOfValues - 1;
   //double genMaxDelta = (type == typeOfReading::SOCReading) ? 0.05 : 0.1;
+  if(values != nullptr){
   for(int i = 0; i < lastButOneIndex; i++) {
      if(!isDifferenceOkay(values[i], values[i + 1], maxDelta[type])) {
              return false;
     }
-  }
+  }}
   return true;
 }
 bool validateSOCreadings(double* values, int numOfValues) {
