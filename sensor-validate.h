a0373a4e36bec5b176101ae2e3d1668007fae9f3
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-03-26 20:04:11 +0530

    Update sensor-validate.h

--- sensor-validate.h
+++ sensor-validate.h
@@ -4,3 +4,8 @@ bool _give_me_a_good_name(double value, double nextValue, double maxDelta);
 bool validateSOCreadings(double* values, int numOfValues);
 
 bool validateCurrentreadings(double* values, int numOfValues);
+
+enum class typeOfReading{
+ SOCReading,
+CurrentReadingc
+};
