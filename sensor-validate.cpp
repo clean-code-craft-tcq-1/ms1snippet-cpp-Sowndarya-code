47fa7e8f01dbca9fac11fda8f103ca009f13d9ef
Sowndarya-code <77658898+Sowndarya-code@users.noreply.github.com>
2021-03-26 20:15:42 +0530

    Update sensor-validate.cpp

--- sensor-validate.cpp
+++ sensor-validate.cpp
@@ -6,23 +6,36 @@ bool _give_me_a_good_name(double value, double nextValue, double maxDelta) {
   }
   return true;
 }
-
-bool validateSOCreadings(double* values, int numOfValues) {
+bool generalValidation(double* values, int numOfValues, typeOfReading type) {
   int lastButOneIndex = numOfValues - 1;
+  double genMaxDelta = (type == SOCReading) ? 0.05 ; 0.1;
   for(int i = 0; i < lastButOneIndex; i++) {
-    if(!_give_me_a_good_name(values[i], values[i + 1], 0.05)) {
-      return false;
+     if(!_give_me_a_good_name(values[i], values[i + 1], genMaxDelta)) {
+             return false;
     }
   }
   return true;
 }
+bool validateSOCreadings(double* values, int numOfValues) {
+  //int lastButOneIndex = numOfValues - 1;
+  //for(int i = 0; i < lastButOneIndex; i++) {
+   // if(!_give_me_a_good_name(values[i], values[i + 1], 0.05)) {
+      //return false;
+    //}
+  //}
+  //return true;
+  validationResult = generalValidation(values, numOfValues, SOCReading);
+  return validationResult;
+}
 
 bool validateCurrentreadings(double* values, int numOfValues) {
-  int lastButOneIndex = numOfValues - 1;
-  for(int i = 0; i < lastButOneIndex; i++) {
-    if(!_give_me_a_good_name(values[i], values[i + 1], 0.1)) {
-      return false;
-    }
-  }
-  return true;
+  //int lastButOneIndex = numOfValues - 1;
+  //for(int i = 0; i < lastButOneIndex; i++) {
+    //if(!_give_me_a_good_name(values[i], values[i + 1], 0.1)) {
+    //  return false;
+    //}
+  //}
+  //return true;
+  validationResult = generalValidation(values, numOfValues, CurrentReading);
+  return validationResult;
 }
